ca26df73020c4666e691bc10cc881139438467fd
Akash <akashpandeyssm71@gmail.com>
2021-11-19 22:28:24 +0530

    Interpolation data binding example img , maths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template:`<div>
+              <img src={{imgString}}/>
               <h1>{{pageHeader}}</h1>
               <app-student></app-student>
+              <p>Salary: {{salary}}</p>
+              <p>10% of Salary: {{salary*.1}}</p>
               </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'HelloWorldApp';
   pageHeader:String="Student Details";
+  imgString:String='https://dotnettutorials.net/wp-content/uploads/2019/09/cropped-dotnettutorials.png';
+  salary:number=500000;
 }
