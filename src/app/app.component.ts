bf2e8e03d5820ac9666066d7711378c2d7c5967f
Akash <akashpandeyssm71@gmail.com>
2021-11-19 22:14:34 +0530

    student component created and nested component example in app-component root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`<div>
+              <h1>{{pageHeader}}</h1>
+              <app-student></app-student>
+              </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyFirstAngularApp';
+  title = 'HelloWorldApp';
+  pageHeader:String="Student Details";
 }
